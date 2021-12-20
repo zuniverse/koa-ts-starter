0f914f729a460a55053cc8d751b26469908946c5
Paul Zuliani <paulzuliani@gmail.com>
2021-12-20 11:51:28 +0100

    Add jest ping.test

---
+++ __tests__/routes/ping.test.ts
@@ -0,0 +1,17 @@
+import server from "../../src/server";
+import request from "supertest";
+
+// close the server after each test
+afterEach(done => {
+  server.close();
+  done();
+});
+
+describe("routes/ping", () => {
+  it("should pong", async () => {
+    const response = await request(server).get("/ping");
+    expect(response.status).toEqual(200);
+    expect(response.type).toEqual("application/json");
+    expect(response.body.data).toEqual("pong");
+  });
+});
